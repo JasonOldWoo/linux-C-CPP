553bddcc9b519f57f2669c4be09ac269af317abb
JasonOldWoo <JasonOldWoo@users.noreply.github.com>
2016-07-20 18:01:44 +0800

    Delete io.hpp

--- deviceagent/deviceagent/io/io.hpp
+++
@@ -1,8 +0,0 @@
-#include "../basedef.h"
-
-namespace zl_device_agent {
-template <typename config, typename type>
-class io : public cbase {
-	typedef typename type::protocol protocol;
-};
-}	// namespace zl_device_agent
